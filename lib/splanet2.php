7cdfe6746d580dae9e329ccf89eee16bb90b406d
tkgstrator <tkgstrator@gmail.com>
2018-09-15 15:23:04 +0900

    Add __FILE__

--- lib/splanet2.php
+++ lib/splanet2.php
@@ -27,44 +27,39 @@ function saveJSON($iksm_session, $type){
     switch($type){
     case "-a":
         // save all battle results
-        $path = dirname(dirname(__FILE__))."/api/results/";
-        echo($path."\n");
+        $path = dirname(__FILE__)."/json/results/";
         check_exists($path);
         $filename = $info["id"].".json";
         $url = "https://app.splatoon2.nintendo.net/api/results";
-        echo("Saving all battle results json...\n");
-        writeJSON($path, $filename, $url, $context);
         break;
     case "-c":
         // save coop schedules
-        $path = dirname(dirname(__FILE__))."/api/coop_schedules/";
+        $path = dirname(__FILE__)."/json/coop_schedules/";
         check_exists($path);
         $filename = $info["id"].".json";
         $url = "https://app.splatoon2.nintendo.net/api/coop_schedules";
-        echo("Saving coop schedules json...\n");
-        writeJSON($path, $filename, $url, $context);
         break;
     case "-d":
         // save a each battle detail
-        $path = dirname(dirname(__FILE__))."/api/results/".$info["id"]."/";
+        $path = dirname(__FILE__)."/json/results/".$info["id"]."/";
         check_exists($path);
-        echo("Saving detailed battle results json...\n");
         for($i=0; $i<50; $i++){
             $filename = ($info["num"]-$i).".json";
             if(file_exists($path.$filename)){
-                echo("Skip ".$filename."(".($i+1)."/50)\n");
                 break;
-            }else{
-                $url = "https://app.splatoon2.nintendo.net/api/results/".($info["num"]-$i);
-                $json = fopen($path.$filename, "w+b");
-                fwrite($json, @file_get_contents($url, false, $context));
-                fclose($json);
-                echo("Save ".$filename."(".($i+1)."/50)\n");
             }
+            $url = "https://app.splatoon2.nintendo.net/api/results/".($info["num"]-$i);
+            $json = fopen($path.$filename, "w+b");
+            fwrite($json, @file_get_contents($url, false, $context));
+            fclose($json);
+            echo("Save ".$filename."(".($i+1)."/50)\n");
         }
+        echo("Done.\n");
+        exit(0);
         break;
     case "-h":
         showhelp();
+        exit(0);
         break;
     case "-i":
         // save images
@@ -79,7 +74,7 @@ function saveJSON($iksm_session, $type){
         ];
         $context = stream_context_create($options);
 
-        $path = dirname(dirname(__FILE__))."/share/".$info["id"]."/";
+        $path = dirname(__FILE__)."/share/".$info["id"]."/";
         check_exists($path);
         for($i=0; $i<50; $i++){
             $filename = ($info["num"]-$i).".png";
@@ -93,10 +88,12 @@ function saveJSON($iksm_session, $type){
             fclose($png);
             echo("Save ".$filename."(".($i+1)."/50)\n");
         }
+        echo("Done.\n");
+        exit(0);
         break;
     case "-l":
         // save a league match ranking
-        $path = dirname(dirname(__FILE__))."/api/league_match_ranking/";
+        $path = dirname(__FILE__)."/json/league_match_ranking/";
         check_exists($path);
         $types = ["P", "T"];
         for($t=1500562800; $t<=time(); $t+=3600*24){
@@ -116,54 +113,47 @@ function saveJSON($iksm_session, $type){
                 }
             }
         }
+        echo("Done.\n");
+        exit(0);
         break;
     case "-m":
         // save merchandises
-        $path = dirname(dirname(__FILE__))."/api/onlineshop/merchandises/";
+        $path = dirname(__FILE__)."/json/merchandises/";
         check_exists($path);
         $filename = $info["id"].".json";
         $url = "https://app.splatoon2.nintendo.net/api/onlineshop/merchandises";
-        echo("Saving marchandises json...\n");
-        writeJSON($path, $filename, $url, $context);
-        break;
-    case "-r":
-        // save user record
-        $path = dirname(dirname(__FILE__))."/api/records/";
-        check_exists($path);
-        $filename = $info["id"].".json";
-        $url = "https://app.splatoon2.nintendo.net/api/records";
-        echo("Saving records json...\n");
-        writeJSON($path, $filename, $url, $context);
-        break;
-    case "-s":
-        // save hero mode results
-        $path = dirname(dirname(__FILE__))."/api/records/hero/";
-        check_exists($path);
-        $filename = $info["id"].".json";
-        $url = "https://app.splatoon2.nintendo.net/api/records/hero";
-        echo("Saving hero mode json...\n");
-        writeJSON($path, $filename, $url, $context);
         break;
     case "-t":
         // save timeline
-        $path = dirname(dirname(__FILE__))."/api/timeline/";
+        $path = dirname(__FILE__)."/json/timeline/";
         check_exists($path);
         $filename = $info["id"].".json";
         $url = "https://app.splatoon2.nintendo.net/api/timeline";
-        echo("Saving timeline json...\n");
-        writeJSON($path, $filename, $url, $context);
+        break;
+    case "-r":
+        // save user record
+        $path = dirname(__FILE__)."/json/records/";
+        check_exists($path);
+        $filename = $info["id"].".json";
+        $url = "https://app.splatoon2.nintendo.net/api/records";
+        break;
+    case "-s":
+        // save hero mode results
+        $path = dirname(__FILE__)."/json/records/hero/";
+        check_exists($path);
+        $filename = $info["id"].".json";
+        $url = "https://app.splatoon2.nintendo.net/api/records/hero";
         break;
     default:
         echo("No such options.\nPlease read the help [-h].\n");
+        exit(0);
         break;
     }
-}
-
-function writeJSON($path, $filename, $url, $context){
-    echo($path.$filename."\n");
+    echo("Saving JSON...\n");
     $json = fopen($path.$filename, "w+b");
     fwrite($json, @file_get_contents($url, false, $context));
     fclose($json);
+    echo("Done!\n");
 }
 
 function check_exists($path){
@@ -187,5 +177,12 @@ function showhelp(){
     echo("-r : save records[total paint point, league stat, stage stat, ...etc].\n");
     echo("-t : save timeline[information of sarmon run, splanet gear shop, ...and more].\n");
     echo("-s : save records of hero mode.\n");
+}
+
+// Main
+if(sizeof($argv) === 3){
+    saveJSON($argv[2], $argv[1]);
+}else{
+    showhelp();
 }
 ?>
